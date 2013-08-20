9e68b8e78529a8c7f76bdf71d02d211edbf273b3
Ronny Chan <ronny6993@gmail.com>
2013-08-20 19:24:11 -0400

    Update tests.java

--- tests.java
+++ tests.java
@@ -2,17 +2,26 @@ import Can
 
 public class tests{
     public static void main(String[] args){
-                String s;
-                Can c;  //declares a variable of type Can
-                c = new Can("aluminum","coca-cola");
-                Can d;
-                d = new Can("skin","very strong acid");
-                d.isCreatedFromHuman = true;
+                //String s; String s is never used. Don't declare unused objects
                 
-                Can e = new Can("titanium","diamonds");
-                e.isCreatedFromHuman = false;
-                Can f = new Can("human skull","blood");
-                f.isCreatedFromHuman = true;
                 
+                //Don't split declarations into 2 lines.
+                //Can c;  //declares a variable of type Can 
+                //c = new Can("aluminum","coca-cola");
+                
+                Can c = new Can("aluminum", "coca-cola", false); //We declare isMadeFromHuman in the object declaration
+                    
+                Can d = new Can("skin","very strong acid", true); 
+                
+                //d.isCreatedFromHuman = true; We have already declared it in object declaration
+                
+                //Do some debugging here
+                System.out.println(c.contents);
+                
+                System.out.println("Can c is "+c.isFull);
+                
+                c.drink();
+                
+                System.out.println("Can c is "+c.isFull);
         }
     }
