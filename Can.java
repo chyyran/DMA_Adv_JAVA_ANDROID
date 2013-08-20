e94bf6280f8b59d160f6101100ef3c8d41b56d55
Ronny Chan <ronny6993@gmail.com>
2013-08-20 19:10:41 -0400

    Update Can.java

--- Can.java
+++ Can.java
@@ -21,21 +21,6 @@ public class Can {
                 isFull = true;
                 isEmpty = false;
         }
-        public static void main(String[] args){
-                String s;
-                Can c;  //declares a variable of type Can
-                c = new Can("aluminum","coca-cola");
-                Can d;
-                d = new Can("skin","very strong acid");
-                d.isCreatedFromHuman = true;
-                
-                Can e = new Can("titanium","diamonds");
-                e.isCreatedFromHuman = false;
-                Can f = new Can("human skull","blood");
-                f.isCreatedFromHuman = true;
-                
-        }
-       
        
         public static Can canMaker(){
                 String a;
